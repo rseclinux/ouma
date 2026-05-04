5595263b374fcde8cae722b00a2da717d711e3b3
Veronika Mikhaylova <theexanori@gmail.com>
2026-05-04 23:51:33 +0300

    unveil mbstate related functions + purge our broken critical_section implementation
    Signed-off-by: Veronika Mikhaylova <theexanori@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
 #![no_std]
-#![allow(
-  unused_macros,
-  non_camel_case_types,
-  non_upper_case_globals,
-  non_snake_case,
-  dead_code
-)]
+#![allow(nonstandard_style, unused_macros, dead_code)]
 #![feature(thread_local, cstr_display, allocator_api)]
 
 #[macro_use]
--- src/std/wchar/mbstate.rs
+++ src/std/wchar/mbstate.rs
@@ -1,34 +1,41 @@
 use {
   crate::{
     MBState,
+    MBStateLock,
     c_char,
     c_int,
-    c_uchar,
     char32_t,
     mbstate_t,
     size_t,
-    std::stdlib,
+    ssize_t,
+    std::{stdio, stdlib, string, uchar},
     support::locale,
     wchar_t,
     wint_t
   },
-  core::ptr
+  core::{ptr, slice},
+  spin::Mutex
 };
 
-/*
 #[unsafe(no_mangle)]
 pub extern "C" fn rs_btowc(c: c_int) -> wint_t {
+  static GLOBAL: Mutex<MBState> = Mutex::new(MBState::new());
+
+  let mut ps = MBStateLock::Owned(GLOBAL.lock());
+  let ctype =
+    locale::get_slot(&locale::get_thread_locale().ctype).unwrap_or_default();
+
   if c == stdio::constants::EOF {
     return super::constants::WEOF;
   }
-  let ctype = locale::get_thread_locale().ctype;
-  let buf: c_char = c as c_char;
+
+  let buf: &[u8] = &[c as u8];
   let mut c32: char32_t = 0;
-  static mut PRIV: mbstate_t = mbstate_t::new();
-  mbstate::mbstate_set_init(ptr::addr_of_mut!(PRIV));
-  if (ctype.mbtoc32)(&mut c32, &buf, 1, ptr::addr_of_mut!(PRIV)) != 1 {
+
+  if (ctype.converter.mbtoc32)(&mut c32, buf, &mut ps) != 1 {
     return super::constants::WEOF;
   }
+
   c32 as wint_t
 }
 
@@ -38,11 +45,22 @@ pub extern "C" fn rs_mbrlen(
   n: size_t,
   ps: *mut mbstate_t
 ) -> size_t {
-  let mut wc: wchar_t = 0;
-  static mut PRIV: mbstate_t = mbstate_t::new();
-  let ps =
-    if !ps.is_null() { unsafe { &mut *ps } } else { ptr::addr_of_mut!(PRIV) };
-  rs_mbrtowc(&mut wc, s, n, ps)
+  static GLOBAL: Mutex<MBState> = Mutex::new(MBState::new());
+  let mut ps = if !ps.is_null() {
+    MBStateLock::Borrowed(unsafe { &mut *ps })
+  } else {
+    MBStateLock::Owned(GLOBAL.lock())
+  };
+  let ctype =
+    locale::get_slot(&locale::get_thread_locale().ctype).unwrap_or_default();
+  let s = unsafe { slice::from_raw_parts(s as *const u8, n) };
+  let mut c32: char32_t = 0;
+
+  let l = (ctype.converter.mbtoc32)(&mut c32, s, &mut ps);
+  if l >= 0 && c32 == '\0' as char32_t {
+    return 0;
+  }
+  l as usize
 }
 
 #[unsafe(no_mangle)]
@@ -52,27 +70,8 @@ pub extern "C" fn rs_mbrtowc(
   n: size_t,
   ps: *mut mbstate_t
 ) -> size_t {
-  let ctype = locale::get_thread_locale().ctype;
-  let mut wc: wchar_t = 0;
-  static mut PRIV: mbstate_t = mbstate_t::new();
-  let ps =
-    if !ps.is_null() { unsafe { &mut *ps } } else { ptr::addr_of_mut!(PRIV) };
-  let (pwc, s, n) = if s.is_null() {
-    (&mut wc as *mut wchar_t, 0 as *const c_char, 1 as size_t)
-  } else if pwc.is_null() {
-    (&mut wc as *mut wchar_t, s, n)
-  } else {
-    (pwc, s, n)
-  };
-  let l = (ctype.mbtoc32)(pwc as *mut char32_t, s, n, ps);
-  unsafe {
-    if l >= 0 && *pwc == '\0' as wchar_t {
-      return 0;
-    }
-  }
-  l as size_t
+  uchar::rs_mbrtoc32(pwc as *mut char32_t, s, n, ps)
 }
-*/
 
 #[unsafe(no_mangle)]
 pub extern "C" fn rs_mbsinit(ps: *const mbstate_t) -> c_int {
@@ -84,86 +83,87 @@ pub extern "C" fn rs_mbsinit(ps: *const mbstate_t) -> c_int {
   }
 }
 
-/*
 #[unsafe(no_mangle)]
-extern "C" fn rs_mbsnrtowcs(
+pub extern "C" fn rs_mbsrtowcs(
   dst: *mut wchar_t,
   src: *mut *const c_char,
-  nmc: size_t,
-  len: size_t,
+  nms: size_t,
   ps: *mut mbstate_t
 ) -> size_t {
-  let ctype = locale::get_thread_locale().ctype;
-  static mut PRIV: mbstate_t = mbstate_t::new();
-  let ps =
-    if !ps.is_null() { unsafe { &mut *ps } } else { ptr::addr_of_mut!(PRIV) };
-  let mut sb: *const c_char = unsafe { *src };
-  let mut nms = nmc;
-  let mut i = len;
-  if dst.is_null() {
-    let mut ret = 0;
-    loop {
-      let mut c32: char32_t = 0;
-      let l = (ctype.mbtoc32)(&mut c32, sb, nms, ps);
-      match l {
-        | -1 => {
-          return -1isize as usize;
-        },
-        | -2 => {
-          return ret;
-        },
-        | _ => {
-          if c32 == 0 {
-            return ret;
-          }
-          sb = sb.wrapping_add(l as usize);
-          nms = nms.wrapping_sub(l as usize);
-          ret += 1;
-        }
-      }
-    }
-  } else {
-    let mut db = dst;
-    while i > 0 {
-      let l = (ctype.mbtoc32)(db as *mut char32_t, sb, nms, ps);
-      match l {
-        | -1 => {
-          unsafe { *src = sb };
-          return -1isize as usize;
-        },
-        | -2 => unsafe {
-          *src = sb.wrapping_add(nms);
-          return db.offset_from(dst) as size_t;
-        },
-        | _ => {
-          unsafe {
-            if *db == 0 {
-              *src = ptr::null_mut();
-              return db.offset_from(dst) as size_t;
-            }
-          }
-          sb = sb.wrapping_add(l as usize);
-          nms = nms.wrapping_sub(l as usize);
-          db = db.wrapping_add(1);
-        }
-      }
-      i -= 1;
-    }
-    unsafe {
-      *src = sb;
-      return db.offset_from(dst) as size_t;
-    }
-  }
+  rs_mbsnrtowcs(dst, src, size_t::MAX, nms, ps)
 }
 
 #[unsafe(no_mangle)]
-extern "C" fn rs_mbsrtowcs(
+pub extern "C" fn rs_mbsnrtowcs(
   dst: *mut wchar_t,
   src: *mut *const c_char,
-  len: size_t,
+  nmc: size_t,
+  nms: size_t,
   ps: *mut mbstate_t
 ) -> size_t {
-  rs_mbsnrtowcs(dst, src, size_t::MAX, len, ps)
+  static GLOBAL: Mutex<MBState> = Mutex::new(MBState::new());
+  let mut ps = if !ps.is_null() {
+    MBStateLock::Borrowed(unsafe { &mut *ps })
+  } else {
+    MBStateLock::Owned(GLOBAL.lock())
+  };
+  let ctype =
+    locale::get_slot(&locale::get_thread_locale().ctype).unwrap_or_default();
+
+  let nmc = unsafe {
+    let max = string::rs_strlen(*src) + 1;
+    nmc.min(max)
+  };
+
+  let s = unsafe { slice::from_raw_parts(*src as *const u8, nmc) };
+  let (mut s_converted, mut d_converted) = (0usize, 0usize);
+
+  while s_converted < nmc && (dst.is_null() || d_converted < nms) {
+    let (result, c32) = if dst.is_null() {
+      let mut c32: char32_t = 0;
+      let l =
+        (ctype.converter.mbtoc32)(&mut c32, &s[s_converted..nmc], &mut ps);
+      (l, c32)
+    } else {
+      let d = unsafe { slice::from_raw_parts_mut(dst as *mut u32, nms) };
+      let l = (ctype.converter.mbtoc32)(
+        &mut d[d_converted],
+        &s[s_converted..nmc],
+        &mut ps
+      );
+      (l, d[d_converted])
+    };
+
+    if result == -1 {
+      if !dst.is_null() {
+        unsafe { *src = s[s_converted..].as_ptr().cast() };
+      }
+      return -1isize as usize;
+    }
+
+    if result == -2 {
+      if !dst.is_null() {
+        unsafe { *src = s[nmc..].as_ptr().cast() };
+      }
+      break;
+    }
+
+    if c32 == '\0' as char32_t {
+      if !dst.is_null() {
+        unsafe { *src = ptr::null_mut() };
+      }
+      return d_converted;
+    }
+
+    d_converted += 1;
+    s_converted += result as usize;
+  }
+
+  if !dst.is_null() {
+    unsafe { *src = s[s_converted..].as_ptr().cast() };
+  }
+
+  d_converted
 }
 
 #[unsafe(no_mangle)]
@@ -172,110 +172,112 @@ pub extern "C" fn rs_wcrtomb(
   wc: wchar_t,
   ps: *mut mbstate_t
 ) -> size_t {
-  let ctype = locale::get_thread_locale().ctype;
-  let mut buf: [c_char; stdlib::constants::MB_LEN_MAX as usize] =
-    [0; stdlib::constants::MB_LEN_MAX as usize];
-  static mut PRIV: mbstate_t = mbstate_t::new();
-  let ps =
-    if !ps.is_null() { unsafe { &mut *ps } } else { ptr::addr_of_mut!(PRIV) };
-  let (s, wc) = if s.is_null() { (buf.as_mut_ptr(), 0) } else { (s, wc) };
-  let l = (ctype.c32tomb)(s, wc as char32_t, ps);
-  if l >= 0 {
-    mbstate::mbstate_set_init(ps);
-  }
-  l as size_t
-}
-
-#[unsafe(no_mangle)]
-pub extern "C" fn rs_wcsnrtombs(
-  dst: *mut c_char,
-  src: *mut *const wchar_t,
-  nwc: size_t,
-  len: size_t,
-  ps: *mut mbstate_t
-) -> size_t {
-  let ctype = locale::get_thread_locale().ctype;
-  static mut PRIV: mbstate_t = mbstate_t::new();
-  let ps =
-    if !ps.is_null() { unsafe { &mut *ps } } else { ptr::addr_of_mut!(PRIV) };
-  let mut len = len;
-  if dst.is_null() {
-    let mut sb: *const char32_t = unsafe { *src as *const char32_t };
-    let mut ret: size_t = 0;
-    let mut nwc = nwc;
-    while nwc != 0 {
-      let mut buf: [c_char; stdlib::constants::MB_LEN_MAX as usize] =
-        [0; stdlib::constants::MB_LEN_MAX as usize];
-      let l =
-        unsafe { (ctype.c32tomb)(buf.as_mut_ptr(), (**src) as char32_t, ps) };
-      if l < 0 {
-        return -1isize as size_t;
-      }
-      ret = ret.wrapping_add(l as usize);
-      unsafe {
-        let s1 = sb;
-        sb = sb.offset(1);
-        if *s1 == '\0' as char32_t {
-          return ret.wrapping_sub(1);
-        }
-      }
-      nwc -= 1;
-    }
-    return ret;
-  } else {
-    let mut db = dst;
-    let mut i = 0;
-    while i < nwc && len > 0 {
-      let mut buf: [c_char; stdlib::constants::MB_LEN_MAX as usize] =
-        [0; stdlib::constants::MB_LEN_MAX as usize];
-      let l =
-        unsafe { (ctype.c32tomb)(buf.as_mut_ptr(), (**src) as char32_t, ps) };
-      if l < 0 {
-        return -1isize as size_t;
-      }
-      if l as usize > len {
-        return unsafe { db.offset_from(dst) as size_t };
-      }
-      let mut k: size_t = 0;
-      while k < l as size_t {
-        unsafe { *db.wrapping_offset(k as isize) = buf[k] };
-        k = k.wrapping_add(1);
-      }
-      unsafe {
-        let s1 = *src;
-        *src = (*src).wrapping_offset(1);
-        if *s1 == 0 {
-          *src = ptr::null();
-          return db.offset_from(dst) as size_t;
-        }
-      }
-      db = db.wrapping_offset(l);
-      len = len.wrapping_sub(l as usize);
-      i += 1;
-    }
-    return unsafe { db.offset_from(dst) as size_t };
-  }
+  uchar::rs_c32rtomb(s, wc as char32_t, ps)
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn rs_wcsrtombs(
   dst: *mut c_char,
   src: *mut *const wchar_t,
-  len: size_t,
+  nms: size_t,
   ps: *mut mbstate_t
 ) -> size_t {
-  rs_wcsnrtombs(dst, src, size_t::MAX, len, ps)
+  rs_wcsnrtombs(dst, src, size_t::MAX, nms, ps)
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn rs_wctob(c: wint_t) -> c_int {
-  let ctype = locale::get_thread_locale().ctype;
-  let buf: [c_char; stdlib::constants::MB_LEN_MAX as usize] =
-    [0; stdlib::constants::MB_LEN_MAX as usize];
-  static mut PRIV: mbstate_t = mbstate_t::new();
-  if (ctype.c32tomb)(buf.as_ptr().cast_mut(), c, ptr::addr_of_mut!(PRIV)) != 1 {
+pub extern "C" fn rs_wcsnrtombs(
+  dst: *mut c_char,
+  src: *mut *const wchar_t,
+  nmc: size_t,
+  nms: size_t,
+  ps: *mut mbstate_t
+) -> size_t {
+  static GLOBAL: Mutex<MBState> = Mutex::new(MBState::new());
+  let mut ps = if !ps.is_null() {
+    MBStateLock::Borrowed(unsafe { &mut *ps })
+  } else {
+    MBStateLock::Owned(GLOBAL.lock())
+  };
+  let ctype =
+    locale::get_slot(&locale::get_thread_locale().ctype).unwrap_or_default();
+
+  let nmc = unsafe {
+    let max = super::rs_wcslen(*src) + 1;
+    nmc.min(max)
+  };
+
+  let s = unsafe { slice::from_raw_parts(*src as *const u32, nmc) };
+  let (mut s_converted, mut d_converted) = (0usize, 0usize);
+  let mut result: ssize_t;
+
+  while s_converted != nmc && (dst.is_null() || d_converted < nms) {
+    let c32: char32_t = s[s_converted];
+    let rem = nms.wrapping_sub(d_converted);
+
+    result = if dst.is_null() {
+      let mut buf: [u8; stdlib::constants::MB_LEN_MAX] =
+        [0; stdlib::constants::MB_LEN_MAX];
+
+      (ctype.converter.c32tomb)(buf.as_mut_slice(), c32)
+    } else {
+      let d = unsafe { slice::from_raw_parts_mut(dst as *mut u8, nms) };
+
+      if rem > ctype.converter.mb_cur_max {
+        (ctype.converter.c32tomb)(&mut d[d_converted..], c32)
+      } else {
+        let mut buf: [u8; stdlib::constants::MB_LEN_MAX] =
+          [0; stdlib::constants::MB_LEN_MAX];
+
+        let l = (ctype.converter.c32tomb)(buf.as_mut_slice(), c32);
+        if l > 0 && l as usize > rem {
+          ps.reset();
+          break;
+        }
+        if l >= 0 {
+          d[d_converted..].copy_from_slice(&buf[..l as usize]);
+        }
+
+        l
+      }
+    };
+
+    if result < 0 {
+      if !dst.is_null() {
+        unsafe { *src = s[s_converted..].as_ptr().cast() };
+      }
+      return -1isize as size_t;
+    }
+
+    if c32 == '\0' as u32 {
+      if !dst.is_null() {
+        unsafe {
+          *src = ptr::null();
+        };
+      }
+      return d_converted;
+    }
+
+    d_converted += result as usize;
+    s_converted += 1;
+  }
+
+  if !dst.is_null() {
+    unsafe { *src = s[s_converted..].as_ptr().cast() };
+  }
+
+  d_converted
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn rs_wctob(c: wint_t) -> c_int {
+  let ctype =
+    locale::get_slot(&locale::get_thread_locale().ctype).unwrap_or_default();
+  let mut buf = [0 as u8; stdlib::constants::MB_LEN_MAX];
+
+  if (ctype.converter.c32tomb)(&mut buf, c as char32_t) != 1 {
     return stdio::constants::EOF;
   }
-  unsafe { *buf.as_ptr() as c_uchar as c_int }
+
+  buf[0] as c_int
 }
-*/

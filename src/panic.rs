9f0fda4342354db26e69ad91d1343470fbabfa26
Veronika Mikhaylova <theexanori@gmail.com>
2026-07-07 16:24:24 +0300

    panic: crash with unique backtrace
    Signed-off-by: Veronika Mikhaylova <theexanori@gmail.com>

--- src/panic.rs
+++ src/panic.rs
@@ -1,6 +1,24 @@
 use core::panic::PanicInfo;
 
+#[inline(always)]
+fn ensure_unique_backtrace() {
+    static ENSURE_UNIQUE_BACKTRACE_INSTANCE: u8 = 0;
+    unsafe {
+        core::arch::asm!(
+            "/* {0} */",
+            in(reg) &ENSURE_UNIQUE_BACKTRACE_INSTANCE,
+            options(nomem, nostack, preserves_flags),
+        );
+    }
+}
+
+#[inline(always)]
+fn crash_with_unique_backtrace() -> ! {
+    ensure_unique_backtrace();
+    core::intrinsics::abort()
+}
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-  loop {}
+  crash_with_unique_backtrace();
 }
